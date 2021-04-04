7184b923416b2e5cdfabccdb0e8736d7af5b90dc
José Rubén Daza Hernández <rudahee@gmail.com>
2021-04-04 21:12:28 +0200

    first commit

---
+++ src/main/java/com/book/MyBookListApplication.java
@@ -0,0 +1,13 @@
+package com.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookListApplication.class, args);
+	}
+
+}
